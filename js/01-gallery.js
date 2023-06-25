51614c219f04c89aec5805994f2b3a3d6c94c5a0
OleksandrLukomskyi <121460896+OleksandrLukomskyi@users.noreply.github.com>
2023-06-25 21:24:17 +0200

    added the code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageCardMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
+
+  //   const galleryEl = evt.target;
+  //   const parentGalleryCard = galleryEl.closest(".gallery__link");
+  //   console.log(parentGalleryCard);
+
+  const imageUrl = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageCardMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
